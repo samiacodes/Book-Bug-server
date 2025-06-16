6ca63c5242e7c582b159f6b16a0ce2f1ec7c8c1f
MDParvezsarkar <mdparvezsarkar10@gmail.com>
2025-06-16 17:31:34 +0600

    cors origin added

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 // MongoDB URI
